7457bde3a4f1dd46cbc96248da862519d85ec810
slsevilla <slsevilla@gmail.com>
2024-02-24 14:09:33 +0000

    chore: rename

--- scripts/core_tree.sh
+++ scripts/core_tree.sh
@@ -39,12 +39,15 @@ eval $(parse_yaml ${pipeline_config} "config_")
 #########################################################
 # set dirs
 log_dir=$output_dir/logs
-intermed_dir=$output_dir/analysis/intermed
-tree_dir=$output_dir/analysis/intermed/tree
+tmp_dir=$output_dir/tmp
+analysis_dir=$output_dir/analysis
+manifest_dir=$log_dir/manifests
+pipeline_dir=$output_dir/tmp/pipeline/tree
 
-pipeline_dir=$output_dir/pipeline
-workingdir=$pipeline_dir/working
-if [[ ! -d $workingdir ]]; then mkdir $workingdir; fi
+intermed_dir=$output_dir/analysis/intermed
+trimm_dir=$tmp_dir/rawdata/trimmed
+tree_dir=$tmp_dir/tree
+gff_dir=$tmp_dir/gff
 
 # set variables
 ODH_version=$config_ODH_version
@@ -52,19 +55,19 @@ phoenix_version=$config_phoenix_version
 dryad_version=$config_dryad_version
 
 # set files
+samplesheet=$log_dir/manifests/samplesheet_gff.csv	
 merged_tree=$intermed_dir/core_genome.tree
 merged_roary=$intermed_dir/core_genome_statistics.txt
 merged_snp=$intermed_dir/snp_distance_matrix.tsv
-samplesheet=$log_dir/manifests/samplesheet_gff.csv	
-	
+
 # set cmd and log
 if [[ $resume == "Y" ]]; then
-	echo "----Resuming pipeline at $workingdir"
+	echo "----Resuming pipeline at $pipeline_dir"
     message_cmd_log "-------Resuming time: `date`"
 	message_cmd_log "-------Resuming space: `df . | sed -n '2 p' | awk '{print $5}'`"
     analysis_cmd=`echo $config_analysis_cmd -resume`
 else
-	echo "----Starting pipeline at $workingdir"
+	echo "----Starting pipeline at $pipeline_dir"
 	message_cmd_log "-------Starting time: `date`"
 	message_cmd_log "-------Starting space: `df . | sed -n '2 p' | awk '{print $5}'`"
     analysis_cmd=$config_analysis_cmd
@@ -72,24 +75,20 @@ fi
 analysis_cmd_trailing=$config_tree_cmd_trailing
 
 #############################################################################################
-# LOG INFO TO CONFIG
-#############################################################################################
-message_cmd_log "--- CONFIG INFORMATION ---"
-message_cmd_log "Analysis date: `date`"
-message_cmd_log "Pipeline version: $ODH_version"
-
-#############################################################################################
 # Analysis
 #############################################################################################
 if [[ $flag_prep == "Y" ]]; then
+	message_cmd_log "------------------------------------------------------------------------"
+	message_cmd_log "--Prepping files"
+	message_cmd_log "------------------------------------------------------------------------"
+
 	# create samplesheet
 	if [[ -f $samplesheet ]]; then rm $samplesheet; fi
 	echo "sample,gff,fq1,fq2" > $samplesheet
-	message_cmd_log "--Prepping GFF files"
 
 	# create sample log
 	if [[ -f sample_list.txt ]]; then rm sample_list.txt; fi
-	for f in $tree_dir/*gff; do
+	for f in $gff_dir/*gff; do
 		filename="${f##*/}"
 		sample_id=`echo $filename | cut -f1 -d"."`
 		echo $sample_id >> sample_list.txt
@@ -101,31 +100,41 @@ if [[ $flag_prep == "Y" ]]; then
 	## create samplesheet
 	## move fq's to CFSAN dir
 	for sample_id in ${sample_list[@]}; do
-		gff="$tree_dir/$sample_id.gff"
-		fq1="${sample_id}_1.trim.fastq.gz"
-		fq2="${sample_id}_2.trim.fastq.gz"
-
+		# set output dir
 		fq_dest="$tree_dir/input_dir/$sample_id"
 		if [[ ! -d $fq_dest ]]; then mkdir -p $fq_dest; fi
 
-		handle_fq $fq_dest $fq1 $tree_dir
-		handle_fq $fq_dest $fq2 $tree_dir
+		# set files
+		gff="$gff_dir/$sample_id.gff"
+		fq1="${sample_id}_1.trim.fastq.gz"
+		fq2="${sample_id}_2.trim.fastq.gz"
+
+		# move files to subdir
+		handle_fq $fq_dest $fq1 $trimm_dir
+		handle_fq $fq_dest $fq2 $trimm_dir
 
 		# add to the samplesheet
         echo "${sample_id},$gff,$fq1,$fq2" >> $samplesheet
 	done
 
-	cat $samplesheet
 	rm sample_list.txt
 fi
 
 if [[ $flag_analysis == "Y" ]]; then
 	#log
+	message_cmd_log "------------------------------------------------------------------------"
+	message_cmd_log "--- CONFIG INFORMATION ---"
+	message_cmd_log "Analysis date: `date`"
+	message_cmd_log "Pipeline version: $ODH_version"
+	message_cmd_log "------------------------------------------------------------------------"
+
+	message_cmd_log "------------------------------------------------------------------------"
 	message_cmd_log "--Creating tree:"
+	message_cmd_log "------------------------------------------------------------------------"
 
     # Run pipeline
-    cd $workingdir
-	pipeline_full_cmd="$analysis_cmd $analysis_cmd_trailing --input $samplesheet --outdir $workingdir --treedir $tree_dir/input_dir --projectID $unique_id"
+    cd $pipeline_dir
+	pipeline_full_cmd="$analysis_cmd $analysis_cmd_trailing --input $samplesheet --outdir $pipeline_dir --treedir $tree_dir/input_dir --projectID $unique_id --kraken2db $config_kraken2_db"
 	echo "$pipeline_full_cmd"
 	$pipeline_full_cmd
 
@@ -135,25 +144,28 @@ if [[ $flag_analysis == "Y" ]]; then
 fi
 
 if [[ $flag_report == "Y" ]]; then
-	cp $workingdir/ROARY/core_genome_statistics.txt $merged_roary
-	cp $workingdir/TREE/core_genome.tree $merged_tree
-	cp $workingdir/CFSAN/snp_distance_matrix.tsv $merged_snp
+	message_cmd_log "------------------------------------------------------------------------"
+	message_cmd_log "--Creating tree:"
+	message_cmd_log "------------------------------------------------------------------------"
+	
+
+	#############################################################################################
+	# Move reports
+	#############################################################################################	
+	cp $pipeline_dir/ROARY/core_genome_statistics.txt $merged_roary
+	cp $pipeline_dir/TREE/core_genome.tree $merged_tree
+	cp $pipeline_dir/CFSAN/snp_distance_matrix.tsv $merged_snp
+	cp $pipeline_batch_dir/pipeline_info/* $log_dir/pipeline
 
 	#############################################################################################
 	# CLEANUP
 	#############################################################################################	
 	if [[ -f $merged_roary ]] && [[ -f $merged_tree ]] && [[ -f $merged_snp ]]; then
 		message_cmd_log "--Pipeline Completed `date`"
-		rm $tree_dir/input_dir/*/*fasta $tree_dir/input_dir/*/*vcf $tree_dir/input_dir/*/*sam $tree_dir/input_dir/*/*txt 
-		rm $tree_dir/input_dir/*/*ba* $tree_dir/input_dir/*/*intervals $tree_dir/input_dir/*/*pile*
-		rm -rf $tree_dir/input_dir/*/metrics
-		tar -zcvf $tree_dir/input_dir.tar.gz $tree_dir/input_dir
+		rm $pipeline_batch_dir
+		rm $trimm_dir
 	else
 		message_cmd_log "--Pipeline FAILED `date`"
 		exit
 	fi
-
-	if  [[ $flag_cleanup == "Y" ]]; then
-		sudo rm -r --force $pipeline_dir
-	fi	
 fi
