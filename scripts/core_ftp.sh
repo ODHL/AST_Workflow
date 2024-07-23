58140de3f8a039e9a8fc116cfe0dc67724c755cf
slsevilla <slsevilla@gmail.com>
2024-07-23 04:50:07 +0000

    feat: add ftp pull

---
+++ scripts/core_ftp.sh
@@ -0,0 +1,9 @@
+flag=$1
+
+if [[ $flag == "1" ]]; then
+	ftp ftp-private.ncbi.nlm.nih.gov
+elif [[ $flag == "2" ]]; then
+	echo "subftp"
+	echo "SniappegEtnurak3"
+	echo "cd uploads/samantha.sevilla_nih.gov_eymkOQSE; mkdir new_dir; cd new_dir"
+fi
