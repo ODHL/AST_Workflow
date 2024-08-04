d82d2f7277a41b2f88d1df82242b95a1adec3b4e
slsevilla <slsevilla@gmail.com>
2024-08-04 20:05:09 +0000

    chore: remove ftp code

--- scripts/core_ftp.sh
+++
@@ -1,9 +0,0 @@
-flag=$1
-
-if [[ $flag == "1" ]]; then
-	ftp ftp-private.ncbi.nlm.nih.gov
-elif [[ $flag == "2" ]]; then
-	echo "subftp"
-	echo "SniappegEtnurak3"
-	echo "cd uploads/samantha.sevilla_nih.gov_eymkOQSE; mkdir new_dir; cd new_dir"
-fi
